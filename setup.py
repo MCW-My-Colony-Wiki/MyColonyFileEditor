67e5537cecf038affd365ecfd3f8f1e313651689
Euxcbsks <hawhaw02030746@gmaail.com>
2021-01-10 18:02:17 +0800

    created reqirements.txt

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 import setuptools
 
-with open('README.md', 'r') as readme:
+with open('README.md', 'r', encoding = 'UTF-8') as readme:
 	long_description = readme.read()
+
+with open('requirements.txt', 'r', encoding = 'UTF-8') as require:
+	requirements = require.read().splitlines()
 
 setuptools.setup(
 	name = 'mcp',
+	url = 'https://github.com/Euxcbsks/mcp',
 	version = '0.0.1',
 	author = 'Euxcbsks',
 	author_email = 'hawhaw02030746@gmail.com',
+	license = 'MIT',
 	description = 'mcp - My Colony Py',
 	long_description = long_description,
 	long_description_content_type = 'text/markdown',
-	url = 'https://github.com/Euxcbsks/mcp',
+	install_requires = requirements,
 	packages = setuptools.find_packages(),
 	classifiers = [
 		'Programming Language :: Python :: 3',
