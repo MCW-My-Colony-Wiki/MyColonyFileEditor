07f56554d4c8369f798a4823fbfa55863904b45a
Euxcbsks <hawhaw02030746@gmail.com>
2021-07-25 23:45:32 +0800

    Completely rewritten progress:     class File(Source): complete     class Category: in progress     class Unit: not yet     config: complete     exceptions: in progress

--- mcp/exceptions.py
+++ mcp/exceptions.py
@@ -1,55 +1,25 @@
-from .tools.info.class_name import class_name
-
-__all__ = [
-	'InvalidSourceError',
-	'InvalidCategoryError',
-	'InvalidUnitError',
-	'PageNotWorkError',
-	'raise_TpE',
-	'raise_ISE',
-	'raise_ICE',
-	'raise_IUE'
-]
-
-class InvalidSourceError(LookupError):
+class MCPerror(Exception):
 	pass
 
-class InvalidCategoryError(LookupError):
+class FileError(MCPerror):
 	pass
+class InvalidVersionChannel(FileError):
+	def __str__(self, channel) -> str:
+		return f"Invalid update channel `{channel}`"
 
-class InvalidUnitError(LookupError):
+class InvalidVersion(FileError):
+	def __str__(self, version) -> str:
+		return f"Invalid version `{version}`"
+
+class InvalidFile(FileError):
+	def __str__(self, file) -> str:
+		return f"Invalid file `{file}`"
+class CacheError(MCPerror):
 	pass
+class InvalidTimeout(CacheError):
+	def __str__(self, timeout) -> str:
+		return f"Invalid timeout `{timeout}`, maybe someone change it?"
 
-class PageNotWorkError(ConnectionError):
-	pass
-
-def raise_TpE(para, *valid_types):
-	if not valid_types:
-		raise ValueError("the 'valid_types' can't be None")
-	
-	def raise_error(para, *valid_types):
-		valid_types_len = len(valid_types)
-		valid_types = [f"'{class_name(Type)}'" if type(Type) != str else f"'{Type}'" for Type in valid_types]
-		
-		if valid_types_len < 2:
-			valid_types = valid_types[0]
-		elif valid_types_len >= 2:
-			valid_types = ", ".join(valid_types[:-1]) + f' or {valid_types[-1]}'
-		
-		raise TypeError(f"the '{para}' must be {valid_types}, not {class_name(para)}")
-	
-	#self check
-	if type(para) != str:
-		raise_error('para', str)
-	
-	#raise it as return
-	raise_error(para, *valid_types)
-
-def raise_ISE(name):
-	raise InvalidSourceError(f"invalid source file '{name}'")
-
-def raise_ICE(name):
-	raise InvalidCategoryError(f"invalid category '{name}'")
-
-def raise_IUE(name):
-	raise InvalidUnitError(f"invalid unit '{name}'")
+class CacheTimeout(CacheError):
+	def __str__(self) -> str:
+		return "This cache is timeout, please update"
